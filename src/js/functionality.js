0b1c29218a1c0d92844bb1cccd80a73d1c583d8f
MattToast <matthew.drozt@gmail.com>
2020-01-21 21:06:18 -0500

    Makes the website more pretty

--- src/js/functionality.js
+++ src/js/functionality.js
@@ -25,30 +25,12 @@ function placeFiles() {
   //   // do something
   // });
 
-  for(i = 0; i < 6; i++) {
-    var div = document.createElement("div");
-
-    var head = document.createElement("p");
-    head.innerHTML = "Here is button #" + i + ":";
-
-    var btn = document.createElement("BUTTON");
-    btn.innerHTML = "Tests";
-
-    div.appendChild(head);
-    div.appendChild(btn); 
-
-    // div.style.float = "left";
-    div.style.width = "100%";
-    // div.style.height = "200%";
-
-    if((i % 2)) {
-      div.style.backgroundColor = "#dddddd";
-      // div.style.float = "right";
-    }
-
-    document.getElementById("files").appendChild(div);
-    // document.getElementById("files").appendChild(btn);
-    // document.getElementById("files").appendChild(head);
+  // Place Buttons for files
+  var table = document.getElementById("files_table");
+  for(i = 0; i < 10; i++) {
+    var row = table.insertRow(1 + i);
+    row.insertCell(0).innerHTML = "File Name";
+    row.insertCell(1).innerHTML = i;
   }
 }
 
