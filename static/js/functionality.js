fbb4bba361f0e196ffd918295a667e05a44605c9
MattToast <matthew.drozt@gmail.com>
2020-01-24 16:08:55 -0500

    Refactor file structure

--- static/js/functionality.js
+++ static/js/functionality.js
@@ -1,7 +1,7 @@
 function main() {
   // What to execute when script is run
   placeFiles();
-  // alert("Done Loading")
+  /// alert(window.location.pathname);
 }
 
 function scrollPage() {
@@ -16,10 +16,10 @@ function placeFiles() {
 
   // Place Buttons for files
   var table = document.getElementById("files_table");
-  for(i = 0; i < 2; i++) {
+  for(i = 0; i < 5; i++) {
     var row = table.insertRow(1 + i);
     row.insertCell(0).innerHTML = "File Name";
-    row.insertCell(1).innerHTML = "<a href=\"./res/img/grad_bg.jpg\">Download</a>";
+    row.insertCell(1).innerHTML = "<a href= \"{{url_for('static', filename='css/master.css')}}\">Download</a>";
   }
 }
 
