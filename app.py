9b1dd7cec540e69365d37740a7bc6398cde7aedb
MattToast <matthew.drozt@gmail.com>
2020-01-26 17:07:37 -0500

    Begin making a page to control files on the website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 import os
 
 app = Flask(__name__)
@@ -13,7 +13,21 @@ def getFiles():
     for file in os.listdir("static/res/share/"):
         listFiles.append(file)
     return jsonify({"files" : listFiles})
+
+@app.route('/control', methods=["GET", "POST"])
+def uploadFiles():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            print(os.path.join("static/res/share/", image.filename))
+            image.save(os.path.join("static/res/share/", image.filename))
+
+            print(image)
+
+            return redirect(request.url) 
+
+    return render_template('control.html')
  
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
     print("\nApplication Terminated")
