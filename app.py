ef735f5bfc4478bbd203c0c0f08122f7d73084f4
MattToast <matthew.drozt@gmail.com>
2020-01-26 21:34:00 -0500

    adds auth required for control page

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect, make_response
+from functools import wraps
 import os
 
 app = Flask(__name__)
+
+
+def check_auth(username, password):
+    return username == 'a' and password == 's'
+
+
+def authenticate():
+    return make_response("Could not verify your login!", 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 
 @app.route("/")
@@ -18,6 +37,7 @@ def getFiles():
 
 
 @app.route('/control', methods=["GET", "POST"])
+@auth_required
 def updateFiles():
     if request.method == "POST":
         if request.files and request.files["upload"]:
@@ -30,9 +50,9 @@ def updateFiles():
             filename = request.form["filename"]
             if os.path.isfile(os.path.join("static/res/share/", filename)):
                 os.remove(os.path.join("static/res/share/", filename))
-            
+
             return redirect(request.url)
-            
+
     return render_template('control.html')
 
 
