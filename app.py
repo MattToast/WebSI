8c39abebba100b76c1a0502653ea3847fab7249d
MattToast <matthew.drozt@gmail.com>
2020-07-30 22:00:11 -0500

    reformat the setup script

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 
 # basic paths to use
-appDir = 'PATH'
+appDir = 'Path'
 shareDir = os.path.join(appDir, "static/res/share/")
 
 
--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ import os
 import hashlib
 import getpass
 
-if __name__ == "__main__":
+
+def main():
+    set_path()
+    set_credentials()
+
+
+def set_path():
     newPath = os.getcwd()
     print("Your current working directory is:")
     print(newPath + "\n")
@@ -15,28 +21,32 @@ if __name__ == "__main__":
         lines = file.readlines()
 
     for index, line in enumerate(lines):
-        if 'appDir = '  in line:
+        if 'appDir = ' in line:
             break
-    
+
     lines[index] = 'appDir = \'' + newPath + '\'\n'
-    
+
     with open('app.py', 'w') as file:
         file.writelines(lines)
 
+
+def set_credentials():
     with open('admin.json', 'r') as admin:
         data = json.load(admin)
-    
-    username = str(hashlib.sha256(input('Please enter an admin username: ').encode()).hexdigest())
+
+    username = str(hashlib.sha256(
+        input('Please enter an admin username: ').encode()).hexdigest())
     password = getpass.getpass('Please enter an admin password: ')
-    
+
     while len(password) < 8:
         print("Password should be 8 or more characters long")
         password = getpass.getpass('Please enter an admin password: ')
 
     password = str(hashlib.sha256(password.encode()).hexdigest())
-    passeord_re_enter = str(hashlib.sha256(getpass.getpass('Please re-enter the password: ').encode()).hexdigest())
+    passeord_re_enter = str(hashlib.sha256(getpass.getpass(
+        'Please re-enter the password: ').encode()).hexdigest())
 
-    if password == passeord_re_enter: 
+    if password == passeord_re_enter:
         data['username'] = username
         data['password'] = password
         print("Username and password have been updated")
@@ -45,3 +55,6 @@ if __name__ == "__main__":
 
     with open('admin.json', 'w') as admin:
         json.dump(data, admin, indent=2)
+
+if __name__ == "__main__":
+    main()
