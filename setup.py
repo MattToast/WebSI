90eb999a84f625f62551c8a19f766f9e58c4e0c6
MattToast <matthew.drozt@gmail.com>
2020-08-05 18:00:58 -0500

    Begin using objects

--- setup.py
+++ setup.py
@@ -5,62 +5,81 @@ import getpass
 
 
 def main():
-    create_missing_dirs()
-    set_admin()
-    design_site()
+    FileStructureSetUp().set_up_files()
 
-def create_missing_dirs():
-    attmept_to_make_dir("./static/res/share/")
-    attmept_to_make_dir("./static/json")
+class ScheduleTable:
+    def __init__(self):
+        self.title = ""
+        self.rows = {}
 
-def attmept_to_make_dir(path):
-    # check that a dir exists, if it doesn't attempt to create it
-    if not os.path.exists(path):
-        try:
-            os.mkdir(path)
-        except Exception:
-            print("Something went wrong creating" + path)
-
-def set_admin():
-    admin_json = {};
-    admin_json['path'] = set_path()
-    (admin_json['username'], admin_json['password']) = set_credentials()
-    with open('./static/json/admin.json', 'w') as admin:
-        json.dump(admin_json, admin, indent=2)
-
-def set_path():
-    newPath = os.getcwd()
-    print("Your current working directory is:")
-    print(newPath + "\n")
-    choice = input("Would you like to use this path (y/n): ")
-    if choice != 'y' and choice != 'Y' and choice != 'yes':
-        newPath = input('Please enter your new path:\n')
-    return newPath
+    def add_row(self):
+        pass
 
 
-def set_credentials():
-    username = str(hashlib.sha256(input('Please enter an admin username: ').encode()).hexdigest())
-    password = getpass.getpass('Please enter an admin password: ')
+class FileStructureSetUp():
+    def __init__(self):
+        pass
 
-    while len(password) < 8:
-        print("Password should be 8 or more characters long")
+    def set_up_files(self):
+        self.create_missing_dirs()
+        self.set_admin()
+        self.design_site()
+
+    def create_missing_dirs(self):
+        self.attmept_to_make_dir("./static/res/share/")
+        self.attmept_to_make_dir("./static/json")
+
+    def attmept_to_make_dir(self, path):
+        # check that a dir exists, if it doesn't attempt to create it
+        if not os.path.exists(path):
+            try:
+                os.mkdir(path)
+            except Exception:
+                print("Something went wrong creating" + path)
+
+    def set_admin(self):
+        admin_json = {}
+        admin_json['path'] = self.set_path()
+        (admin_json['username'], admin_json['password']) = self.set_credentials()
+        with open('./static/json/admin.json', 'w') as admin:
+            json.dump(admin_json, admin, indent=2)
+
+    def set_path(self):
+        newPath = os.getcwd()
+        print("Your current working directory is:")
+        print(newPath + "\n")
+        choice = input("Would you like to use this path (y/n): ")
+        if choice != 'y' and choice != 'Y' and choice != 'yes':
+            newPath = input('Please enter your new path:\n')
+        return newPath
+
+    def set_credentials(self):
+        username = str(hashlib.sha256(
+            input('Please enter an admin username: ').encode()).hexdigest())
         password = getpass.getpass('Please enter an admin password: ')
 
-    password = str(hashlib.sha256(password.encode()).hexdigest())
-    passeord_re_enter = str(hashlib.sha256(getpass.getpass('Please re-enter the password: ').encode()).hexdigest())
+        while len(password) < 8:
+            print("Password should be 8 or more characters long")
+            password = getpass.getpass('Please enter an admin password: ')
 
-    if password == passeord_re_enter:
-        print("Username and password have been updated")
-        return (username, password)
-    else:
-        print("Passwords do not match, changes are being ignored")
-        exit()
+        password = str(hashlib.sha256(password.encode()).hexdigest())
+        passeord_re_enter = str(hashlib.sha256(getpass.getpass(
+            'Please re-enter the password: ').encode()).hexdigest())
 
-def design_site():
-    design_json = {}
-    design_json['title'] = input("Please enter a title for your site: ")
-    with open('./static/json/design.json', 'w') as design:
-        json.dump(design_json, design, indent=2)
+        if password == passeord_re_enter:
+            print("Username and password have been updated")
+            return (username, password)
+        else:
+            print("Passwords do not match, changes are being ignored")
+            exit()
+
+    def design_site(self):
+        design_json = {}
+        design_json['title'] = input("Please enter a title for your site: ")
+        table = ScheduleTable()
+        with open('./static/json/design.json', 'w') as design:
+            json.dump(design_json, design, indent=2)
+
 
 if __name__ == "__main__":
     main()
